532bb556b047472bda537a94ad5404741deb4013
Aurélien Pora <aurelien.pora@grenoble-inp.org>
2018-05-15 18:06:17 +0200

    decoupage en mcu peut etre

--- etu/include/decoupage_mcu.h
+++ etu/include/decoupage_mcu.h
@@ -8,10 +8,19 @@
 struct mcu;
 
 
-/*permet de recuperer les échantillons RGB à partir du fichier
-d’entrée PPM premiere case type du tableau deuxieme case largeur troisieme case
-hauteur*/
+
+struct mcu *decoupage_mcu(const char *ppm_filename, int8_t h1, int8_t v1);
+
+/* permet de recuperer les échantillons RGB à partir du fichier
+ * d’entrée PPM premiere case type du tableau deuxieme case largeur troisieme case
+ * hauteur */
 int8_t *recuperation_rgb(const char *ppm_filename);
+
+/*retourne POUR L'INSTANT QU'UN TABLEAU DE 64 */
+int *tableau_decoupe(*tab_rgb);
+
+/**/
+struct mcu *renvoie_mcu_8_8(*tableau_decoupe);
 
 
 #endif /* decoupage_mcu */
--- etu/src/decoupage_mcu.c
+++ etu/src/decoupage_mcu.c
@@ -4,15 +4,17 @@
 #include <string.h>
 // #include "decoupage_mcu.h"
 
-struct MCU{
-  uint8_t Y[64];
-  uint8_t Cb[64];
-  uint8_t Cr[64];
+struct mcu{
+  int8_t h;
+  int8_t v;
+  int8_t rgb[64];
+  int8_t y;
+  int8_t cb;
+  int8_t cr;
 };
 
-
-int *recuperation_rgb(const char *ppm_filename){
-  // uint64_t type_noir;
+uint8_t *recuperation_rgb(const char *ppm_filename)
+{
   char *type;
   type = malloc(2*sizeof(char));
   char *largeur;
@@ -34,18 +36,68 @@ int *recuperation_rgb(const char *ppm_filename){
   fread(stderr, 1, 5, ppm);
   if (!strcmp(type, "P5"))
   {
-    uint8_t tableau[atoi(hauteur)*atoi(largeur) + 3];
-    tableau[0] = atoi(type);
+    uint8_t *tableau = malloc(atoi(hauteur)*atoi(largeur) + 3);
+    // 1 car 1 pixel = 1 byte
+    tableau[0] = 1;
     tableau[1] = atoi(largeur);
     tableau[2] = atoi(hauteur);
-    fread(tableau+3, 1, tableau[1]*tableau[2], ppm);
+    fread(tableau + 3, 1, tableau[1]*tableau[2], ppm);
+    fclose(ppm);
     return tableau;
   }
-  fclose(ppm);
-
 }
 
+
+// int *tableau_decoupe(int*tab_rgb)
+// {
+//   if (tab_rgb[0]*tab_rgb[1]*tab_rgb[2] == 64)
+//   {
+//     return(tab_rgb);
+//   }
+// }
+
+struct mcu decoupage_mcu(const char *ppm_filename, int8_t h1, int8_t v1)
+{
+  if(h1 == 1 && v1 == 1)
+  {
+    uint8_t *tab_rgb = recuperation_rgb(ppm_filename);
+
+    if (tab_rgb[0] == 1)
+    //cas négro
+    {
+      if (tab_rgb[1] % 8 == 0 && tab_rgb[2] % 8 ==0) // on fait juste le cas multiple de 8
+      {
+        struct mcu tableau_de_mcu[tab_rgb[2]/8][tab_rgb[1]/8];
+        for (int32_t k = 0; k < tab_rgb[2]*tab_rgb[1]; k++)
+        {
+          int32_t i = (k/tab_rgb[2])/8-1;
+          int32_t j = (k/tab_rgb[2])/8-1;
+          int32_t i_prime = k/tab_rgb[2] - 8*i;
+          int32_t j_prime = k%tab_rgb[2] -j*8;
+          tableau_de_mcu[i][j].rgb[i_prime*8+j_prime] = tab_rgb[k + 3];
+          printf("%hhx %i\n", tableau_de_mcu[i][j].rgb[i_prime*8+j_prime], k);
+        }
+      }
+    }
+  }
+}
+
+// struct mcu *create_mcu(const char *ppm_filename)
+// {
+//
+// }
+
+
+
+// struct mcu *renvoie_mcu_8_8(*tableau_decoupe)
+// {
+//   if(tableau_decoupe[0] == 1)
+//   {
+//     struct mcu *
+//   }
+// }
+
 int main(int argc, char const *argv[]) {
-  recuperation_rgb("/user/6/poraa/Downloads/Encodeur-JPEG-master/Encodeur-JPEG/etu/images/invader.pgm");
+  decoupage_mcu("/user/6/poraa/Downloads/Encodeur-JPEG-master/Encodeur-JPEG/etu/images/gris.pgm",1,1);
   return 0;
 }
