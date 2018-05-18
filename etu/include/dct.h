16c5e740390bf9fa54e7f6ca64520bcd17347c1f
Marion Taconné <Marion.Taconne@grenoble-inp.org>
2018-05-18 11:33:53 +0200

    petite modif

--- etu/include/dct.h
+++ etu/include/dct.h
@@ -8,6 +8,13 @@
 #include <math.h>
 #include "decoupage_mcu.h"
 
+
+/*
+Transformation en cosinus discrete (DCT):
+prend en parametre la composante a transformer ainsi qu'une composante vide
+(qui deviendra la composante transformee)
+*/
+
 extern void dct_bloc(uint8_t *composante, int16_t *nouvelle_composante);
 
 
