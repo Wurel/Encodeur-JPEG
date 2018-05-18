71d39187d42268f1f5eb5ededd2dd7d7957050ec
Guillaume Caulier <Guillaume.Caulier@grenoble-inp.org>
2018-05-18 11:32:38 +0200

    quelques commentaires

--- etu/include/AC_DC.h
+++ etu/include/AC_DC.h
@@ -5,13 +5,18 @@
 #include "bitstream.h"
 #include <stdint.h>
 
+
+//Retourne la magnitude d'un nombre entré en paramètre
 int8_t retourne_magnitude(int16_t nombre);
 
+//Retourne la place d'un nombre entré en paramètre dans sa classe de magnitude
 int16_t retourne_bits(int16_t nombre, int8_t magnitude);
 
-void ecriture(struct bitstream *stream, uint16_t nombre);
+
+void ecriture_symbole_DC(struct bitstream *stream, uint16_t nombre);
 
 void AC_composante_puis_huffman(struct bitstream *stream, int16_t *composante);
 
+void ecriture_symbole_AC(struct bitstream *stream, uint32_t symbole_decode, uint8_t *nbits);
 
 #endif /* AC_DC */
