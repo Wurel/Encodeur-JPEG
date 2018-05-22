16c76b97c48d46b320261b4d64a3420f753c305f
Guillaume Caulier <Guillaume.Caulier@grenoble-inp.org>
2018-05-22 17:03:59 +0200

    down sampler debut

---
+++ etu/include/down_sampler.h
@@ -0,0 +1,13 @@
+#ifndef down_sampler
+#define down_sampler
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "structures.h"
+
+extern void echantillonage_horizontal(struct mcu);
+
+#endif /* down_sampler */
